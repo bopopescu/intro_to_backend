2e813e54b88448c69770dc90e8c98902959680b3
doctor3397 <hs2546@columbia.edu>
2017-03-29 16:45:22 -0400

    Rot13

--- Rot13/rot13.py
+++ Rot13/rot13.py
@@ -2,7 +2,7 @@ import os
 import re
 from string import letters
 
-import jinja2 
+import jinja2
 import webapp2
 
 from google.appengine.ext import db
@@ -13,27 +13,27 @@ jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), aut
 
 
 class Handler(webapp2.RequestHandler):
-    def write(self, *a, **kw):
-        self.response.write(*a, **kw)
-        
     def render_str(self, template, **params):
         t = jinja_env.get_template(template)
         return t.render(params)
-    
+
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
-        
+
+    def write(self, *a, **kw):
+        self.response.write(*a, **kw)
 
 class Rot13(Handler):
     def get(self):
         self.render("rot13.html")
-        
+
     def post(self):
+        rot13 = ""
         text = self.request.get('text')
-        if text: 
+        if text:
             rot13 = text.encode('rot13')
         self.render('rot13.html', text = rot13)
-    
+
 # URL mapping handler
 app = webapp2.WSGIApplication([
     ('/unit2/rot13', Rot13)],
